bdf2bfd66fe72a8dce0efec110c3b04c244c4784
Shalzz <shaleen.jain95@gmail.com>
2013-06-10 12:56:07 +0530

    Added option to select heap size

--- tools/sign.bat
+++ tools/sign.bat
@@ -2,5 +2,4 @@
 
 ECHO I: Signing...
 set /p heap=<tools\heapsize.txt
-echo %heap%
 java -Xmx%heap%m -jar tools\signapk.jar -w tools\testkey.x509.pem tools\testkey.pk8 "%name%.apk"  "%name%_signed.apk"
